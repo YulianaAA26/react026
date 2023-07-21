38611030c71f1c63ec5f4dab4c52bd21a1ce7123
Yuliana Aguilar Apaza <yutervalink.26@gmail.com>
2023-07-21 01:58:58 -0500

    v03 tarea

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import C01component from './component/C01component';
+import C02contador from './component/C02contador';
+import C03dobleestado from './component/C03dobleestado';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Component 01</h1>
+      <C01component></C01component>
+      <h1>Component 02</h1>
+      <C02contador></C02contador>
+      <h1>Component 03</h1>
+      <C03dobleestado></C03dobleestado>
     </div>
   );
 }
